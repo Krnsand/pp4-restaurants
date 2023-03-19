86c5e4f89e853aa8063ae1a1f7eae7e6f080cba2
Krnsand <krnsandin@gmail.com>
2023-03-19 16:19:13 +0000

    Begin Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+class Booking(models.Model):
+    booking_id = models.AutoField(max_length=10, unique=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='booking_id')
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, unique=True)
+    rst_location = models.CharField()
+    date_time = models.DateTimeField()
+
+
+
+
+    class Meta():
+
+    def __str__(self):
+        return self.title
