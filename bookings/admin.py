b1081a67881e4d5dbad5cca2243197ec436ce29e
Krnsand <krnsandin@gmail.com>
2023-03-20 09:24:34 +0000

    Create admin panel

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_filter = ('rst_location', 'date', 'time')
+    summernote_fields = ('comment')
