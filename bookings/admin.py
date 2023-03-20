0286988adebeb4ccba5c0bcbe47ba10729afd308
Krnsand <krnsandin@gmail.com>
2023-03-20 14:19:42 +0000

    Add search fields to admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,5 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
 
+    list_display = ('rst_location', 'date', 'time')
+    search_fields = ['rst_location', 'date', 'time']
     list_filter = ('rst_location', 'date', 'time')
     summernote_fields = ('comment')
