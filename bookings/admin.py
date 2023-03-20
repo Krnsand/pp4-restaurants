72d3b861f3b39b446549cc6b734056288ac0df4f
Krnsand <krnsandin@gmail.com>
2023-03-20 15:15:27 +0000

    Rename rst_location to restaurant in admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, User
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
 
-    list_display = ('rst_location', 'date', 'time')
-    search_fields = ['rst_location', 'date', 'time']
-    list_filter = ('rst_location', 'date', 'time')
+    list_display = ('restaurant', 'date', 'time')
+    search_fields = ['restaurant', 'date', 'time']
+    list_filter = ('restaurant', 'date', 'time')
     summernote_fields = ('comment')
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin)
