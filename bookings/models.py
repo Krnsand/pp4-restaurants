3315b92c62d3dd50c73d65877f0f0f5fcae0d7a3
Krnsand <krnsandin@gmail.com>
2023-03-20 15:16:09 +0000

    Add guest and restaurant choices in models

--- bookings/models.py
+++ bookings/models.py
@@ -29,19 +29,43 @@ TIME_CHOICES = (
 )
 
 
+GUEST_CHOICES = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+    ("6", "6"),
+    ("7", "7"),
+    ("8", "8"),
+    ("9", "9"),
+    ("10", "10"),
+)
+
+
+RST_CHOICES = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+)
+
+
 class Booking(models.Model):
     booking_id = models.IntegerField(unique=True, primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='booking_id')
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(max_length=100, unique=True)
-    rst_location = models.CharField(max_length=100)
-    date = models.DateField(max_length=10, default=timezone.now)
-    time = models.TimeField(max_length=10, choices=TIME_CHOICES, default=timezone.now)
-    comment = models.TextField(max_length=200, blank=True, default=False)
+    restaurant = models.CharField(max_length=100, choices=RST_CHOICES, blank=True)
+    date = models.DateField(max_length=10, default=timezone.now, blank=True)
+    time = models.TimeField(max_length=10, choices=TIME_CHOICES, default=timezone.now, blank=True)
+    guests = models.IntegerField(choices=GUEST_CHOICES, default=False, blank=True)
+    comment = models.TextField(max_length=200, default=False, blank=True)
 
 # class Meta():
 
     def __str__(self):
-        return self.first_name
+        return 'Booking for {party} at {restaurant} on {date}'.format(party=self.party_size,
+                                                                      restaurant=self.restaurant,
+                                                                      date=self.time)
 
