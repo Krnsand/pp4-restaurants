92be58dd7129831e454323a2816f6cd1cebbf020
Krnsand <krnsandin@gmail.com>
2023-03-24 14:22:13 +0000

    Add restaurants and settings to models as classes

--- bookings/models.py
+++ bookings/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
+from django.db.models import IntegerField, Model
 
 
 # Create your models here.
@@ -56,11 +57,25 @@ class Booking(models.Model):
     time = models.TimeField(max_length=10, choices=TIME_CHOICES, default=timezone.now, blank=True)
     guests = models.IntegerField(choices=GUEST_CHOICES, default=False, blank=True)
     comment = models.TextField(max_length=200, default=False, blank=True)
+    # created_on = models.DateTimeField(auto_now=True)
 
-# class Meta():
+    # class Meta():
+    #    """To display the recipes by created_on in descending order"""
+    #    ordering = ['-created_on']
 
     def __str__(self):
         return 'Booking for {{ guests }} at {{ restaurant }} on {{ date }}'.format(party=self.party_size,
                                                                       restaurant=self.restaurant,
                                                                       date=self.time)
 
+
+class Restaurants(models.Model):
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100)
+    settings_id = models.IntegerField(default=False)
+
+
+class Settings():
+    opens = models.IntegerField()
+    closes = models.IntegerField()
+    active_mon_tues = models.BooleanField(default=False)
