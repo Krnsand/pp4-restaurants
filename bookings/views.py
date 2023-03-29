2dd94ddf2330f82d0a67a71c5a3ece08262f8310
Krnsand <krnsandin@gmail.com>
2023-03-29 17:41:10 +0000

    Create view for add_reservation

--- bookings/views.py
+++ bookings/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import View, ListView, DetailView
 from .models import Booking
+# from .forms import RestaurantForm
 
 
 # look over this
@@ -11,9 +12,12 @@ from .models import Booking
 #    def get(request):
 #        return render(request, 'index.html')
 
+def add_reservation(request):
+    from = RestaurantForm
+    return render(request, 'add_reservation.html', {'form':form})
 
 class BookingList(generic.ListView):
     model = Booking
     queryset = Booking.objects.filter()
-    template_name = "index.html"
+    template_name = "base.html"
     context_object_name = 'booking_list'
