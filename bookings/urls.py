9f02880953cdbaf25ddf58b3f2bdac8b903b2516
Krnsand <krnsandin@gmail.com>
2023-03-29 17:41:29 +0000

    Create path in urls to add_reservation

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
 from .views import BookingList
+# from .views import HomePage
 
 urlpatterns = [
     # path("", views.HomePage, name="home"),
     path("", views.BookingList.as_view(), name="bookings"),
     # path("", views.Booking, name='bookings'),
+    path('add_reservation', views.add_reservation, name="add-reservation"),
 ]
