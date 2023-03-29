4b9d713887525382d16cebb73d613e971b79d256
Krnsand <krnsandin@gmail.com>
2023-03-29 16:10:46 +0000

    Still trying to link to my bookings page

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .views import BookingList
 
 urlpatterns = [
+    # path("", views.HomePage, name="home"),
     path("", views.BookingList.as_view(), name="bookings"),
-    path("", views.Booking, name='bookings'),
+    # path("", views.Booking, name='bookings'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,25 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView
 from .models import Booking
 
 
-# class TableListView(ListView):
-#    model = Table
-#    queryset = Table.objects.filter()
-#    template_name = 'index.html'
-
-
-# class TableDetailView(DetailView):
-#    model = Table
-#    template_name = 'table_detail.html'
-
-
 # look over this
+# class HomePage(View):
+#    def get(request):
+#        return render(request, 'index.html')
+
+
 class BookingList(generic.ListView):
     model = Booking
     queryset = Booking.objects.filter()
-    template_name = "bookings.html"
+    template_name = "index.html"
     context_object_name = 'booking_list'
