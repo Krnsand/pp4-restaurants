37996287e03379b8bd159427cf1cce0587569199
Krnsand <krnsandin@gmail.com>
2023-03-30 16:22:14 +0000

    Clean up forms with PEP8

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,26 +6,51 @@ from .models import Booking
 
 # Create a Restaurant booking form
 
+
 class RestaurantForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = Booking
-        fields = ('restaurant', 'name', 'email', 'date',
-                  'time', 'guests', 'comment')
+        fields = (
+            "restaurant",
+            "name",
+            "email",
+            "date",
+            "time",
+            "guests",
+            "comment",
+        )
         labels = {
-            'restaurant': '',
-            'name': '',
-            'email': '',
-            'date': '',
-            'time': '',
-            'guests': '',
-            'comment': '',
+            "restaurant": "",
+            "name": "",
+            "email": "",
+            "date": "",
+            "time": "",
+            "guests": "",
+            "comment": "",
         }
         widgets = {
-            'restaurant': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Restaurant'}),
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
-            'date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Date'}),
-            'time': forms.TimeInput(attrs={'class': 'form-control', 'placeholder': 'Time'}),
-            'guests': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '# of Guests'}),
-            'comment': forms.Textarea(attrs={'class': 'custom-form-field', 'placeholder': 'Comment anything we might need to know for your visit'}),
+            "restaurant": forms.Select(
+                attrs={"class": "form-control", "placeholder": "Restaurant"}
+            ),
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Your Name"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"class": "form-control", "placeholder": "Your Email"}
+            ),
+            "date": forms.Select(
+                attrs={"class": "form-control", "placeholder": "Date"}
+            ),
+            "time": forms.SelectDateWidget(
+                attrs={"class": "form-control", "placeholder": "Time"}
+            ),
+            "guests": forms.Select(
+                attrs={"class": "form-control", "placeholder": "# of Guests"}
+            ),
+            "comment": forms.Textarea(
+                attrs={
+                    "class": "custom-form-field",
+                    "placeholder": "Comment anything we might need to know for your visit",
+                }
+            ),
         }
