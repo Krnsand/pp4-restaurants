f5f1ffca740839e0b6a8d130128c268d44018885
Krnsand <krnsandin@gmail.com>
2023-03-30 10:25:13 +0000

    Create urls for violet, green, orange

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import BookingList
+from .views import BookingList, VioletFusion, GreenDining, OrangeCushion
 # from .views import HomePage
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", views.BookingList.as_view(), name="bookings"),
     # path("", views.Booking, name='bookings'),
     path('add_reservation', views.add_reservation, name="add-reservation"),
+    path('violet_fusion', views.VioletFusion.as_view(), name="violet"),
+    path('green_dining', views.GreenDining.as_view(), name="green"),
+    path('orange_cushion', views.OrangeCushion.as_view(), name="orange"),
 ]
