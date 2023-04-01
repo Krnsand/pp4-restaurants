fe60d42cc96faeded4b9c5020afeab1d7f335960
Krnsand <krnsandin@gmail.com>
2023-04-01 17:43:06 +0000

    Add card to style to booking details

--- restaurant_concept/settings.py
+++ restaurant_concept/settings.py
@@ -129,7 +129,7 @@ DATABASES = {
  }
 
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
 
