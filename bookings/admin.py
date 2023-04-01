84a48f81104171567d1606d3eaf7bd637abc1f65
Krnsand <krnsandin@gmail.com>
2023-04-01 07:42:18 +0000

    Add user to display/filter field in admin panel

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,17 +6,12 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
-    list_display = ("restaurant", "date", "time")
-    search_fields = ["restaurant", "date", "time"]
-    list_filter = ("restaurant", "date", "time")
+    list_display = ("user", "restaurant", "date", "time")
+    search_fields = ["user", "restaurant", "date", "time"]
+    list_filter = ("user", "restaurant", "date", "time")
     summernote_fields = "comment"
 
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     pass
-
-
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#    pass
